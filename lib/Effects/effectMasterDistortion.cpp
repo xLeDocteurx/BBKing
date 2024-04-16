132781a1ff9b61fc363b64c628210d6348fed814
clenoir <christopher.lenoir@webinage.fr>
2024-04-16 17:54:00 +0200

    Breaks task handlers but segment the code into lib folder

--- lib/Effects/effectMasterDistortion.cpp
+++ lib/Effects/effectMasterDistortion.cpp
@@ -1,13 +1,8 @@
 #include <Defs.h>
 
-#ifndef EFFECTMASTERDISTORTION_H
-#define EFFECTMASTERDISTORTION_H
-
 void masterEffectDistortion(State* statePointer)
 {
     for (int i = 0; i < PLAY_WAV_WAV_BUFFER_SIZE; i++) {
         statePointer->_masterBuffer[i] = statePointer->_masterBuffer[i];
     }
 }
-
-#endif // EFFECTMASTERDISTORTION_H
