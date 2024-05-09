e0aa73b4ed7e7a5a7706daf7913a5d98b937cde2
clenoir <christopher.lenoir@webinage.fr>
2024-05-10 01:49:12 +0200

    started working on master effects

---
+++ lib/Effects/effectMasterCompressor.cpp
@@ -0,0 +1,15 @@
+#include <Defs.h>
+
+void masterEffectCompressor(int16_t *sample)
+{
+    float threshold = 0.5;
+    // float ratio = 1 / 2;
+    int ratio = 2;
+    float q = 0.5;
+    int attackInMs = 20;
+    int releaseInMs = 200;
+    
+    float dryWetRatio = 0.75;
+    float inputGain = 1.0;
+    float outputGain = 1.0;
+}
